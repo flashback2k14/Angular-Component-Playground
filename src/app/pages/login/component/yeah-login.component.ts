4e6e01deacb07b5c78cb858ba99f842d4bd0d7bd
Sebastian Kloppe <feedbackcalcest@gmail.com>
2019-06-17 13:36:01 +0200

    change tabs styling

--- src/app/pages/login/component/yeah-login.component.ts
+++ src/app/pages/login/component/yeah-login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { DataService } from "../../../core/services/data.service";
+import { DataService } from "../../../services/data.service";
 
 @Component({
   selector: "yeah-login",
